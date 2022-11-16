efd1312f7dded6d918003237ddfb715c95d153a3
SzymanskiKarol <szymanski.karol.1991@gmail.com>
2022-11-16 22:43:58 +0100

    Bootstrap updated

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import os
 
 app = Flask(__name__)
 app.app_context().push()
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SECRET_KEY'] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 ckeditor = CKEditor(app)
 bootstrap=Bootstrap5(app)
 
